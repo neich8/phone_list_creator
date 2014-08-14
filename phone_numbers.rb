976a156bd9f8b0558e344b4a38400c74735331b0
neich8 <neich0888@gmail.com>
2014-08-13 20:00:24 -0500

    Phone numbers

---
+++ phone_numbers.rb
@@ -0,0 +1,22 @@
+require 'faker'
+require 'csv'
+file_locale = ARGV
+counter = 0
+
+def file_location()
+	if ARGV.length > 0
+		return ARGV[0] 
+	else
+		return "../"
+	end
+end
+
+CSV.open("#{file_location}phonenumbers.csv", "wb") do |csv|
+	while counter < 100 
+		csv << [Faker::Name.first_name,
+				Faker::Name.last_name,
+				Faker::PhoneNumber.cell_phone,
+				Faker::Company.name]
+		counter += 1
+	end
+end
